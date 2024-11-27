5bc813da8cfcb92ca0c456fdb87174e7a8dd074e
Rong-yyc <2324923940@qq.com>
2024-11-27 19:01:44 +0800

    04_day_03/04 and modify 04_day_02

--- 04_day/section3/bootpack.c
+++ 04_day/section3/bootpack.c
@@ -5,11 +5,15 @@ void write_mem8(int addr, int data);
 /* 是函数声明，却不用{}，而是直接 ; ，这表示函数是在别的文件中，你自己找一下把*/
 void HariMain(void)
 {
-    int i; // 变量声明：i 是一个32位整数
+    int i;   // 变量声明：i 是一个32位整数
+    char *p; // 变量 p 是用于内存地址的专用变量，用于 byte 型地址
 
     for (i = 0xa0000; i <= 0xaffff; i++)
     {
-        write_mem8(i, i & 0x0f); /* MOV BYTE [i], i&0x0f*/
+        p = (char *)i;
+        *p = i & 0x0f; /* MOV BYTE [p],i & 0x0f*/
+
+        /*以上就可以替代 write_mem8(i, i & 0x0f);*/
     }
 
     for (;;)
---
+++ 04_day/section4/bootpack.c
@@ -0,0 +1,22 @@
+/* 告诉 C 编译器，函数在别的文件里 */
+void io_hlt(void);
+void write_mem8(int addr, int data);
+
+/* 是函数声明，却不用{}，而是直接 ; ，这表示函数是在别的文件中，你自己找一下把*/
+void HariMain(void)
+{
+    int i;             // 变量声明：i 是一个32位整数
+    char *p = 0xa0000; // 变量 p 是用于内存地址的专用变量，用于 byte 型地址
+
+    for (i = 0; i <= 0xffff; i++)
+    {
+        *(p + i) = i & 0x0f; /* MOV BYTE [p+i],i & 0x0f*/
+
+        /*以上就可以替代 write_mem8(i, i & 0x0f);*/
+    }
+
+    for (;;)
+    {
+        io_hlt();
+    }
+}
